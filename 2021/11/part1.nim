367243bb1d8ce2cdb88ae227d4f2d570a5c27745
Paweł Obrok <yapee@yapee.pl>
2021-12-12 06:06:29 +0100

    Solve 2021.11

---
+++ 2021/11/part1.nim
@@ -0,0 +1,37 @@
+import sequtils
+import strutils
+import parseutils
+import bitops
+import sets
+import tables
+import options
+
+type Point = tuple[x: int, y: int]
+
+var octopi = stdin.readAll.strip.split("\n").mapIt(it.toSeq.mapIt(int(it) - int('0')))
+var flashes = 0
+
+proc flash(octopi: var seq[seq[int]], flashed: var HashSet[Point], x: int, y: int) =
+  if not flashed.contains((x, y)) and octopi[x][y] >= 10:
+    flashed.incl((x, y))
+
+    for x2 in max(octopi.low, x - 1) .. min(octopi.high, x + 1):
+      for y2 in max(octopi[0].low, y - 1) .. min(octopi[0].high, y + 1):
+        if x2 != x or y2 != y:
+          octopi[x2][y2] += 1 
+          octopi.flash(flashed, x2, y2)
+
+for _ in 1 .. 100:
+  var flashed = initHashSet[Point]()
+  for x in octopi.low .. octopi.high:
+    for y in octopi[0].low .. octopi[0].high:
+      octopi[x][y] += 1
+      octopi.flash(flashed, x, y)
+
+  for x in octopi.low .. octopi.high:
+    for y in octopi[0].low .. octopi[0].high:
+      if octopi[x][y] >= 10:
+        octopi[x][y] = 0
+        flashes += 1
+
+echo flashes
